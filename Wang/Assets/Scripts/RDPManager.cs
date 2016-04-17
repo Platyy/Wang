48f9aebb8b57f9c497da4617f167a0a02876a888
Nick <nicholasdallafiore@hotmail.com>
2016-04-17 21:14:02 +1000

    More organization

---
+++ Wang/Assets/Scripts/RDPManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RDPManager : MonoBehaviour {
+
+    /*
+        Used to keep track of the RDP this script is attached to.
+    */
+
+    private int m_PineAmount = 0, m_WoodAmount = 0, m_StoneAmount = 0, m_IronAmount = 0;
+    private Vector3 m_Position;
+}
